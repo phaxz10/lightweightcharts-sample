e67c6157f8bd3c88b722d8d0e64c6e125b050762
unknown <phaxz.bautista10@gmail.com>
2021-08-22 17:55:53 +0800

    first take on lightweightcharts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { createChart } from 'lightweight-charts';
 import './App.css';
+import React, { useRef, useEffect } from 'react';
+import data from './BTCUSD.json';
 
 function App() {
+  const chartContainer = useRef();
+  const chart = useRef();
+
+  useEffect(() => {
+    chart.current = createChart(chartContainer.current, {
+      width: chartContainer.current.offsetWidth,
+      height: chartContainer.current.offsetHeight,
+    });
+    const candleSeries = chart.current.addCandlestickSeries();
+    candleSeries.setData(data);
+
+    const handleClick = (param) => {
+      if (!param.point) {
+        return;
+      }
+      const price = param.seriesPrices.get(candleSeries);
+
+      alert(price.close);
+    };
+
+    chart.current.subscribeClick(handleClick);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div ref={chartContainer} style={{ height: '100vh', width: '100vw' }}>
+      {/* <div ref={chartContainer} className='chart-container' /> */}
     </div>
   );
 }
